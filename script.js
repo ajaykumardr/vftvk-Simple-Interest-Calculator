26a0ebcf76d48fec041f9d5b4d2a0aff33d827a1
ajaykumardr <52170657+ajaykumardr@users.noreply.github.com>
2022-05-06 21:44:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    t = document.getElementById("years").value;
+    r = document.getElementById("rate").value;
+
+    si = (p*t*r)/100;
+
+    var template =
+	'<p>If you deposit' + " " + '<mark>' + p + '</mark>' + "," + '</p>' + 
+	'<p>at an interest rate of'  + " " + '<mark>' + r + '% </mark>' + '</p>' +
+    '<p>You will recieve an amount of' + " " + '<mark>' + si + '</mark>' + "," + '</p>' + 
+    '<p>in the year' + " " + '<mark>'+ (new Date().getFullYear() + parseInt(t)) + '</mark>' + "."+ '</p>';
+
+    document.getElementById("result").innerHTML = template;
+}
+
+function show_value(value) {
+    document.getElementById("slider_value").innerHTML = value + "%";
 }
         
