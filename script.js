0236fef0fb64f68f3984232e223952dad11d7e76
ajaykumardr <52170657+ajaykumardr@users.noreply.github.com>
2022-05-06 22:45:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    t = document.getElementById("years").value;
-    r = document.getElementById("rate").value;
+    
+    if(p>0)
+    {
+        t = document.getElementById("years").value;
+        r = document.getElementById("rate").value;
 
-    si = (p*t*r)/100;
+        si = (p*t*r)/100;
 
-    var template =
-	'<p>If you deposit' + " " + '<mark>' + p + '</mark>' + "," + '</p>' + 
-	'<p>at an interest rate of'  + " " + '<mark>' + r + '% </mark>' + '</p>' +
-    '<p>You will recieve an amount of' + " " + '<mark>' + si + '</mark>' + "," + '</p>' + 
-    '<p>in the year' + " " + '<mark>'+ (new Date().getFullYear() + parseInt(t)) + '</mark>' + "."+ '</p>';
+        var template =
+	    '<p>If you deposit' + " " + '<mark>' + p + '</mark>' + "," + '</p>' + 
+	    '<p>at an interest rate of'  + " " + '<mark>' + r + '% </mark>' + '</p>' +
+        '<p>You will recieve an amount of' + " " + '<mark>' + si + '</mark>' + "," + '</p>' + 
+        '<p>in the year' + " " + '<mark>'+ (new Date().getFullYear() + parseInt(t)) + '</mark>' + "."+ '</p>';
 
-    document.getElementById("result").innerHTML = template;
+        document.getElementById("result").innerHTML = template;
+
+        document.getElementById("result").style.visibility = 'visible';        
+
+    }
+    else if (p==0)
+    {
+        alert('Principal amount cannot be zero');
+        document.getElementById("principal").focus();
+    }
+    else if(p < 0)
+    {
+        alert('Principal amount cannot be negative');
+        document.getElementById("principal").focus();
+    }
 }
 
 function show_value(value) {
     document.getElementById("slider_value").innerHTML = value + "%";
 }
-        
